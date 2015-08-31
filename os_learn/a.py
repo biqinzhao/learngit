afbb77cd3d1a5221a0670d6e200af97e930db00e
Wangxuemin <xuemin.wang@memblaze.com>
2015-08-31 11:19:11 +0800

    "this python learn process"

---
+++ os_learn/a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 
+import os
+print os.getcwd()
+print os.listdir('/home')
+print os.system('dir')
+#print os.rename('RE','learn_re')
+print os.path.split('os_learn.py')
+print os.path.exists('/home')
+print os.path.isabs('/home')
